deed1f6182df0859d7653674feabb57c62e362e2
Tim Neville <tim.neville@veeva.com>
2023-07-17 11:56:26 -0400

    base url function

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,16 +1,16 @@
 import axios from 'axios'
 
-const BASE_URL =
+export const getBaseUrl = (version = 1) =>
   process.env.NODE_ENV === 'production'
-    ? process.env.REACT_APP_PROD_API_URL
-    : process.env.REACT_APP_DEV_API_URL
+    ? process.env.REACT_APP_PROD_API_URL + `/v${version}`
+    : process.env.REACT_APP_DEV_API_URL + '/v1'
 
 export default axios.create({
-  baseURL: BASE_URL,
+  baseURL: getBaseUrl(),
 })
 
 export const axiosPrivate = axios.create({
-  baseURL: BASE_URL,
+  baseURL: getBaseUrl(),
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 })
